5defbb1b31b5ceb3201ef55cf247cbef5c2c3ff4
Balajibv1997 <mailID>
2023-07-12 13:21:52 +0530

    commit 1

---
+++ HouseRentalApplication/src/test/java/Refrence/Reff.java
@@ -0,0 +1,22 @@
+package Refrence;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Reff {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+	System.out.println("hi");
+	}
+}	
+
+
+
